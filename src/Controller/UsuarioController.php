215995a7f235737b2b2fbbb0bb40004abc379880
Guillemat09 <160737040+Guillemat09@users.noreply.github.com>
2025-09-29 18:31:11 +0200

    ejemploPlantillas

--- src/Controller/UsuarioController.php
+++ src/Controller/UsuarioController.php
@@ -3,17 +3,17 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class UsuarioController extends AbstractController
 {
     #[Route('/usuario', name: 'app_usuario')]
-    public function index(): JsonResponse
+    public function index(): Response
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/UsuarioController.php',
+        return $this->render('usuario/index.html.twig', [
+            'titulo' => 'GymBro Fitness Center',
+            'descripcion' => 'El mejor gimnasio de la ciudad, equipado con la última tecnología y entrenadores profesionales para ayudarte a alcanzar tus objetivos.',
         ]);
     }
 }
